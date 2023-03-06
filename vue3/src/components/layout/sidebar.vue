371cecaa6f9d73907477f23b7f340185d7b88779
Malachai <prussian1933@naver.com>
2023-03-06 17:35:10 +0900

    initialized branch <develop>

---
+++ vue3/src/components/layout/sidebar.vue
@@ -0,0 +1,113 @@
+<template>
+    <!--  BEGIN SIDEBAR  -->
+    <div class="sidebar-wrapper sidebar-theme">
+        <nav ref="menu" id="sidebar">
+            <div class="shadow-bottom"></div>
+
+            <perfect-scrollbar class="list-unstyled menu-categories" tag="ul" :options="{ wheelSpeed: 0.5, swipeEasing: !0, minScrollbarLength: 40, maxScrollbarLength: 300, suppressScrollX: true }">
+                <li class="menu">
+                    <router-link to="/" class="dropdown-toggle" @click="toggleMobileMenu">
+                        <div class="">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="feather feather-target"
+                            >
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <circle cx="12" cy="12" r="6"></circle>
+                                <circle cx="12" cy="12" r="2"></circle>
+                            </svg>
+                            <span>{{ $t('google') }}</span>
+                        </div>
+                    </router-link>
+                </li>
+                <li class="menu">
+                    <router-link to="/index2" class="dropdown-toggle" @click="toggleMobileMenu">
+                        <div class="">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="feather feather-target"
+                            >
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <circle cx="12" cy="12" r="6"></circle>
+                                <circle cx="12" cy="12" r="2"></circle>
+                            </svg>
+                            <span>{{ $t('kakao') }}</span>
+                        </div>
+                    </router-link>
+                </li>
+                <li class="menu">
+                    <router-link to="/index2" class="dropdown-toggle" @click="toggleMobileMenu">
+                        <div class="">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                stroke-width="2"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                class="feather feather-target"
+                            >
+                                <circle cx="12" cy="12" r="10"></circle>
+                                <circle cx="12" cy="12" r="6"></circle>
+                                <circle cx="12" cy="12" r="2"></circle>
+                            </svg>
+                            <span>{{ $t('naver') }}</span>
+                        </div>
+                    </router-link>
+                </li>
+            </perfect-scrollbar>
+        </nav>
+    </div>
+    <!--  END SIDEBAR  -->
+</template>
+
+<script setup>
+    import { onMounted, ref } from 'vue';
+    import { useStore } from 'vuex';
+    const store = useStore();
+
+    const menu_collapse = ref('dashboard');
+
+    onMounted(() => {
+        const selector = document.querySelector('#sidebar a[href="' + window.location.pathname + '"]');
+        if (selector) {
+            const ul = selector.closest('ul.collapse');
+            if (ul) {
+                let ele = ul.closest('li.menu').querySelectorAll('.dropdown-toggle');
+                if (ele) {
+                    ele = ele[0];
+                    setTimeout(() => {
+                        ele.click();
+                    });
+                }
+            } else {
+                selector.click();
+            }
+        }
+    });
+
+    const toggleMobileMenu = () => {
+        if (window.innerWidth < 991) {
+            store.commit('toggleSideBar', !store.state.is_show_sidebar);
+        }
+    };
+</script>
