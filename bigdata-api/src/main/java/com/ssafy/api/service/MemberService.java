c0d2b634613f97352fe27172c22ef167fb144114
ingkoon <dlswotmd@gmail.com>
2023-03-13 17:28:26 +0900

    Feature: member service

--- bigdata-api/src/main/java/com/ssafy/api/service/MemberService.java
+++ bigdata-api/src/main/java/com/ssafy/api/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.ssafy.api.service;
 
 import com.ssafy.api.common.token.JwtTokenProvider;
+import com.ssafy.api.dto.member.exception.DuplicateMemberException;
 import com.ssafy.api.dto.member.exception.NoSuchMemberException;
 import com.ssafy.api.dto.member.request.MemberLoginRequest;
 import com.ssafy.api.dto.member.request.MemberSignUpRequest;
@@ -13,6 +14,7 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,30 +29,43 @@ public class MemberService {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final JwtTokenProvider jwtTokenProvider;
     private final StringRedisTemplate stringRedisTemplate;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public void signUp(MemberSignUpRequest memberSignUpRequest){
-        Member member = memberSignUpRequest.toEntity();
+    public void signUp(MemberSignUpRequest request){
+        isExistEmail(request.getEmail());
+        String password = passwordEncoder.encode(request.getPassword());
+        Member member = request.toEntity(password);
         memberRepository.save(member);
     }
 
     public MemberResponse login(MemberLoginRequest request){
+
         Member member = memberRepository.findByEmail(request.getEmail()).orElseThrow(NoSuchMemberException::new);
+        log.info(member.getEmail());
 
         // 1. Login ID/PW 를 기반으로 Authentication 객체 생성
         // 이때 authentication 는 인증 여부를 확인하는 authenticated 값이 false
         UsernamePasswordAuthenticationToken authenticationToken = request.toAuthentication();
 
+        log.info("============ access authentication Token ==============");
+
         // 2. 실제 검증 (사용자 비밀번호 체크)이 이루어지는 부분
         // authenticate 매서드가 실행될 때 CustomUserDetailsService 에서 만든 loadUserByUsername 메서드가 실행
         Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        log.info("============ access authentication ==============");
 
         // 3. 인증 정보를 기반으로 JWT 토큰 생성
         MemberResponse tokenInfo = jwtTokenProvider.generateToken(authentication, member, true, "");
+        log.info("============ access token ==============");
 
         // 4. RefreshToken Redis 저장 (expirationTime 설정을 통해 자동 삭제 처리)
         stringRedisTemplate.opsForValue()
                 .set("RT:" + member.getId(), tokenInfo.getRefreshToken(), tokenInfo.getRefreshTokenExpirationTime(), TimeUnit.MILLISECONDS);
         return tokenInfo;
+    }
+
+    public void isExistEmail(String email){
+        if(memberRepository.existsByEmail(email)) throw new DuplicateMemberException();
     }
 }
