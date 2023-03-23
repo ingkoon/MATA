1a4e2b5c3dae7f1e4d42f6b678677de5557a963d
Malachai <prussian1933@naver.com>
2023-03-24 01:57:43 +0900

    Feat: run.sh

--- hadoop-cluster/run.sh
+++ hadoop-cluster/run.sh
@@ -83,4 +83,77 @@ sudo docker exec -d master02 sh -c 'cassandra -R >> $CASSANDRA_HOME/cassandra-st
 sleep 10 && \
 sudo docker exec -d slave01 sh -c 'cassandra -R >> $CASSANDRA_HOME/cassandra-startup.log' && \
 \
-sudo docker exec -d master02 kafka-topics.sh --create --topic tagmanager --replication-factor 1 --partitions 1 --bootstrap-server localhost:9092
+sudo docker exec master02 kafka-topics.sh --create --topic tagmanager --replication-factor 1 --partitions 1 --bootstrap-server localhost:9092
+\
+sleep 10 && \
+sudo docker exec master01 cqlsh master01 9042 -e "
+CREATE KEYSPACE tagmanager WITH REPLICATION = {
+	'class' : 'SimpleStrategy',
+	'replication_factor' : 1
+};
+USE tagmanager;
+CREATE TABLE stream (
+  key TEXT,
+  service_token TEXT,
+  client_id BIGINT,
+  service_id BIGINT,
+  session_id TEXT,
+  event TEXT,
+  target_id TEXT,
+  position_x INT,
+  position_y INT,
+  location TEXT,
+  prev_location TEXT,
+  referrer TEXT,
+  creation_timestamp TIMESTAMP,
+  age_duration BIGINT,
+  PRIMARY KEY ((service_id), creation_timestamp, session_id)
+) WITH CLUSTERING ORDER BY (creation_timestamp DESC);
+" && \
+sudo docker exec master01 hive -e "
+CREATE DATABASE mata;
+USE mata;
+CREATE TABLE IF NOT EXISTS services(
+  service_id BIGINT,
+  host_name STRING,
+  PRIMARY KEY(service_id) DISABLE NOVALIDATE
+) STORED AS ORC;
+CREATE TABLE IF NOT EXISTS mata.components(
+  total_click INT,
+  target_id STRING,
+  location STRING,
+  update_timestamp TIMESTAMP,
+  service_id BIGINT,
+  CONSTRAINT fk_service_id FOREIGN KEY(service_id) REFERENCES mata.services(service_id) DISABLE NOVALIDATE 
+) CLUSTERED BY (service_id) SORTED BY (update_timestamp DESC) INTO 32 BUCKETS
+STORED AS ORC;
+CREATE TABLE IF NOT EXISTS mata.clicks(
+  total_click INT,
+  position_x INT,
+  position_y INT,
+  location STRING,
+  update_timestamp TIMESTAMP,
+  service_id BIGINT,
+  CONSTRAINT fk_service_id FOREIGN KEY(service_id) REFERENCES mata.services(service_id) DISABLE NOVALIDATE 
+) CLUSTERED BY (service_id) SORTED BY (update_timestamp DESC) INTO 32 BUCKETS
+STORED AS ORC;
+CREATE TABLE IF NOT EXISTS mata.page_durations(
+  total_duration BIGINT,
+  total_session INT,
+  location STRING,
+  update_timestamp TIMESTAMP,
+  service_id BIGINT,
+  CONSTRAINT fk_service_id FOREIGN KEY(service_id) REFERENCES mata.services(service_id) DISABLE NOVALIDATE 
+) CLUSTERED BY (service_id) SORTED BY (update_timestamp DESC) INTO 32 BUCKETS
+STORED AS ORC;
+CREATE TABLE IF NOT EXISTS mata.page_durations(
+  total_duration BIGINT,
+  total_session INT,
+  location STRING,
+  update_timestamp TIMESTAMP,
+  service_id BIGINT,
+  CONSTRAINT fk_service_id FOREIGN KEY(service_id) REFERENCES mata.services(service_id) DISABLE NOVALIDATE 
+) CLUSTERED BY (service_id) SORTED BY (update_timestamp DESC) INTO 32 BUCKETS
+STORED AS ORC;
+"
+
