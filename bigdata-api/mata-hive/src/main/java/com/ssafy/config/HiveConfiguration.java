2eb452e152695f312358575d9a84e9e1d59ed8ee
ingkoon <dlswotmd@gmail.com>
2023-03-23 12:46:55 +0900

    Feature: Hive 연동 관련 설정 수정

--- bigdata-api/mata-hive/src/main/java/com/ssafy/config/HiveConfiguration.java
+++ bigdata-api/mata-hive/src/main/java/com/ssafy/config/HiveConfiguration.java
@@ -1,16 +1,24 @@
 package com.ssafy.config;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
 
+@Slf4j
+@RequiredArgsConstructor
 @Configuration
 public class HiveConfiguration {
+
+    @Value("${hive.driver-class-name}")
+    private String driverClassName;
     @Value("${hive.url}")
     private String hiveUrl;
 
@@ -23,14 +31,14 @@ public class HiveConfiguration {
     @Bean
     public DataSource hiveDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.apache.hive.jdbc.HiveDriver");
+        dataSource.setDriverClassName(driverClassName);
         dataSource.setUrl(hiveUrl);
         dataSource.setUsername(hiveUsername);
         dataSource.setPassword(hivePassword);
         return dataSource;
     }
 
-    @Bean
+    @Bean(name = "jdbcTemplate")
     public JdbcTemplate hiveJdbcTemplate() {
         return new JdbcTemplate(hiveDataSource());
     }
--- bigdata-api/mata-hive/src/main/java/com/ssafy/repository/HiveRepository.java
+++ bigdata-api/mata-hive/src/main/java/com/ssafy/repository/HiveRepository.java
@@ -2,7 +2,6 @@ package com.ssafy.repository;
 
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -16,7 +15,7 @@ public class HiveRepository {
     private final JdbcTemplate jdbcTemplate;
 
     public List<Map<String, Object>> selectData() {
-        String sql = "SELECT * FROM weblogs";
+        String sql = "show databases";
         return jdbcTemplate.queryForList(sql);
     }
 }
