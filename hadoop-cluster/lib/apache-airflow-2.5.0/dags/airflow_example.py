8138dd93e314fc5b3ff41239989f8c95e14f686a
junho <blacktick@naver.com>
2023-03-23 10:40:14 +0900

    Feat: airflow spark --ing

--- hadoop-cluster/lib/apache-airflow-2.5.0/dags/airflow_example.py
+++ hadoop-cluster/lib/apache-airflow-2.5.0/dags/airflow_example.py
@@ -1,25 +1,41 @@
-from airflow import DAG
-from airflow.operators.bash_operator import BashOperator
-from datetime import datetime, timedelta
+# from airflow import DAG
+# from airflow.operators.bash_operator import BashOperator
+# from datetime import datetime, timedelta
+#
+# default_args = {
+#     'owner': 'my_username',
+#     'depends_on_past': False,
+#     'start_date': datetime(2023, 3, 22),
+#     'email': ['my_email@example.com'],
+#     'email_on_failure': False,
+#     'email_on_retry': False,
+#     'retries': 1,
+#     'retry_delay': timedelta(minutes=1),
+# }
+#
+# dag = DAG('cassandra_to_hive', default_args=default_args, schedule_interval=timedelta(minutes=1))
+#
+# with dag:
+#     t1 = BashOperator(
+#         task_id='run_spark_job',
+#         bash_command='spark-submit $AIRFLOW_HOME/dags/lib/spark/spark_job.py ',
+#         dag=dag
+#     )
+#
+# t1
+
+from lib.spark.spark_job import read_cassandra_to_spark
 
 default_args = {
-    'owner': 'my_username',
-    'depends_on_past': False,
-    'start_date': datetime(2023, 3, 22),
-    'email': ['my_email@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=1),
+    'owner': 'airflow',
+    'start_date': datetime(2023, 3, 23)
 }
 
-dag = DAG('cassandra_to_hive', default_args=default_args, schedule_interval=timedelta(minutes=1))
+with DAG('CassandraToHive', default_args=default_args, schedule_interval=None) as dag:
 
-with dag:
-    t1 = BashOperator(
-        task_id='run_spark_job',
-        bash_command='spark-submit /path/to/spark_job.py',
-        dag=dag
+    read_cassandra_to_spark = PythonOperator(
+        task_id='read_cassandra_to_spark',
+        python_callable=read_cassandra_to_spark
     )
 
-t1
+    ead_cassandra_to_spark
--- hadoop-cluster/lib/apache-airflow-2.5.0/dags/lib/spark/spark_job.py
+++ hadoop-cluster/lib/apache-airflow-2.5.0/dags/lib/spark/spark_job.py
@@ -1,25 +1,74 @@
-# 필요한 라이브러리 import
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, count, sum
+# import findspark
+# findspark.init("/usr/local/lib/spark-3.3.2-bin-hadoop3")
+#
+# # 필요한 라이브러리 import
+# from pyspark.sql import SparkSession
+# from pyspark.sql.functions import col, count, sum
+#
+# cassandra_keyspace = "tagmanager"
+# cassandra_table = "stream"
+#
+# # SparkSession 생성
+# spark = SparkSession.builder \
+#                     .appName("Cassandra to Hive") \
+#                     .master("yarn") \
+#                     .config("spark.sql.catalogImplementation","hive") \
+#                     .enableHiveSupport() \
+#                     .getOrCreate()
+#
+# # Cassandra 데이터 읽기
+# df = spark.read \
+#          .format("org.apache.spark.sql.cassandra") \
+#          .option("spark.cassandra.connection.host", "master01") \
+#          .option("spark.cassandra.connection.port", 9042) \
+#          .options(table=cassandra_table, keyspace= cassandra_keyspace) \
+#          .load()
+#
+# # 데이터 집계
+# agg_df = df.groupBy("position_x", "location") \
+#            .agg(count("*").alias("count"), sum("bytes").alias("total_bytes"))
+#
+#
+# # Hive 테이블 생성
+# spark.sql("CREATE TABLE IF NOT EXISTS web_logs_agg(position_x STRING, location STRING, position_x_count BIGINT, total_bytes BIGINT) \
+#            USING hive")
+#
+# # Hive에 데이터 저장
+# agg_df.write \
+#       .mode("append") \
+#       .saveAsTable("web_logs_agg")
 
-# SparkSession 생성
-spark = SparkSession.builder \
-                    .appName("Cassandra to Hive") \
-                    .config("spark.sql.catalogImplementation","hive") \
-                    .enableHiveSupport() \
-                    .getOrCreate()
 
-# Cassandra 데이터 읽기
-df = spark.read \
-         .format("org.apache.spark.sql.cassandra") \
-         .options(table="web_logs", keyspace="my_keyspace") \
-         .load()
+import findspark
+findspark.init("/usr/local/lib/spark-3.3.2-bin-hadoop3")
 
-# 데이터 집계
-agg_df = df.groupBy("date", "url") \
-           .agg(count("*").alias("count"), sum("bytes").alias("total_bytes"))
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from pyspark.sql import *
 
-# Hive에 데이터 저장
-agg_df.write \
-      .mode("overwrite") \
-      .saveAsTable("web_logs_agg")
+
+def read_cassandra_to_spark():
+    # Cassandra connection details
+    cassandra_keyspace = 'tagmanager'
+    cassandra_table = 'stream'
+
+    # Spark session
+    spark = SparkSession.builder.appName('CassandraToHive').getOrCreate()
+
+    # Create Cassandra dataframe
+    cassandra_df = spark.read.format("org.apache.spark.sql.cassandra") \
+                    .option("spark.cassandra.connection.host", "master01") \
+                    .option("spark.cassandra.connection.port", 9042) \
+                    .option("keyspace", cassandra_keyspace) \
+                    .option("table", cassandra_table) \
+                    .load()
+
+    # Create Hive schema
+    schema = StructType([
+        StructField("id", IntegerType(), True),
+        StructField("name", StringType(), True)
+    ])
+
+    # Create Hive table
+    hive_table = 'hive_table'
+    cassandra_df.write.format("hive").option("table", hive_table).option("schema", "default").save()
