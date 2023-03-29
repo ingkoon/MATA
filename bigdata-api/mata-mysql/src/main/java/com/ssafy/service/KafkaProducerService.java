64131c89f615da2c899692c5a691f85a8289dcda
ingkoon <dlswotmd@gmail.com>
2023-03-29 12:50:27 +0900

    Feature: 웹로그 내 서비스 아이디 추가 로직 작성

--- bigdata-api/mata-mysql/src/main/java/com/ssafy/service/KafkaProducerService.java
+++ bigdata-api/mata-mysql/src/main/java/com/ssafy/service/KafkaProducerService.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
@@ -28,7 +29,7 @@ public class KafkaProducerService {
     private final KafkaTemplate<String, String> template;
     private final Validation validation;
     private final ProjectRepository projectRepository;
-
+    private final StringRedisTemplate redisTemplate;
 
     public void sendToKafka(final WebLog data) throws JsonProcessingException {
         final ProducerRecord<String, String> record = data.toProducerRecord(TOPIC_TAG_MANAGER, 0);
@@ -52,6 +53,14 @@ public class KafkaProducerService {
         if(!validation.checkRedisValidation(token)){
             Project project = projectRepository.findByToken(token).orElseThrow(RedisKeyExecption::new);
         };
+    }
 
+    public Long getProjectId(String token){
+        String id = redisTemplate.opsForValue().get(token);
+        if(id == null){
+            Project project = projectRepository.findByToken(token).orElseThrow(RedisKeyExecption::new);
+            return project.getId();
+        }
+        return Long.parseLong(id);
     }
 }
