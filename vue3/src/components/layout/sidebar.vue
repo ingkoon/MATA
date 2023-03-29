9865192c69765709c5bace436a42aa23a6550077
dsrla123 <dsrla123@naver.com>
2023-03-29 19:52:15 +0900

    feat: 서비스 목록 done

--- vue3/src/components/layout/sidebar.vue
+++ vue3/src/components/layout/sidebar.vue
@@ -5,7 +5,7 @@
             <div class="shadow-bottom"></div>
 
             <perfect-scrollbar class="list-unstyled menu-categories" tag="ul" :options="{ wheelSpeed: 0.5, swipeEasing: !0, minScrollbarLength: 40, maxScrollbarLength: 300, suppressScrollX: true }">
-                <li class="menu">
+                <li class="menu" v-for="(item,index) in payload" :key="index">
                     <router-link to="/" class="dropdown-toggle" @click="toggleMobileMenu">
                         <div class="">
                             <svg
@@ -24,56 +24,11 @@
                                 <circle cx="12" cy="12" r="6"></circle>
                                 <circle cx="12" cy="12" r="2"></circle>
                             </svg>
-                            <span>{{ $t('google') }}</span>
+                            <span>{{item.name}}</span>
                         </div>
                     </router-link>
                 </li>
-                <li class="menu">
-                    <router-link to="/index2" class="dropdown-toggle" @click="toggleMobileMenu">
-                        <div class="">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                class="feather feather-target"
-                            >
-                                <circle cx="12" cy="12" r="10"></circle>
-                                <circle cx="12" cy="12" r="6"></circle>
-                                <circle cx="12" cy="12" r="2"></circle>
-                            </svg>
-                            <span>{{ $t('kakao') }}</span>
-                        </div>
-                    </router-link>
-                </li>
-                <li class="menu">
-                    <router-link to="/index2" class="dropdown-toggle" @click="toggleMobileMenu">
-                        <div class="">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                stroke="currentColor"
-                                stroke-width="2"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                class="feather feather-target"
-                            >
-                                <circle cx="12" cy="12" r="10"></circle>
-                                <circle cx="12" cy="12" r="6"></circle>
-                                <circle cx="12" cy="12" r="2"></circle>
-                            </svg>
-                            <span>{{ $t('naver') }}</span>
-                        </div>
-                    </router-link>
-                </li>
+                
                 <li class="menu">
                     <router-link to="/users/add-app" class="dropdown-toggle" @click="toggleMobileMenu">
                         <div class="">
@@ -97,8 +52,8 @@
                         </div>
                     </router-link>
                 </li>
-                <v-for>
-                <>
+            
+                
             </perfect-scrollbar>
         </nav>
     </div>
@@ -106,12 +61,12 @@
 </template>
 
 <script setup>
-    import { onMounted, ref } from 'vue';
+    import { onMounted, ref, onBeforeMount } from 'vue';
     import { useStore } from 'vuex';
     import VueJwtDecode from 'vue-jwt-decode'
     import axios from 'axios';
     const store = useStore();
-
+    const payload=ref([])
     const menu_collapse = ref('dashboard');
     function getProjectList(){
             // console.log(token)
@@ -127,21 +82,26 @@
             
             })
               .then(res=>{
-              console.log(res)
-              const payload=(res.data)
+              console.log(`axios done ${res}`,res)
+              payload.value=res.data
+              console.log('asd')
               this.$store.dispatch('get_service_list',payload)
-
+              return payload
               })
               .catch(err=>{
               console.log(err.response)
               })
           }
+    // onBeforeMount(() => {
+    //     getProjectList()
+    // }),
     onMounted(() => {
         console.log("sidebar mount start")
         getProjectList()
-        // console.log(store.state.token)
-        // const uid=VueJwtDecode.decode(store.state.token)
-        // console.log("uid"+uid)
+        // console.log('getProjectList:', getProjectList())
+        // const payload=getProjectList()
+        // console.log(`payload: ${payload}`)
+    
         const selector = document.querySelector('#sidebar a[href="' + window.location.pathname + '"]');
         if (selector) {
             const ul = selector.closest('ul.collapse');
