6af34dcc4407659f1c88fa9d61516539bb14ba4b
ingkoon <dlswotmd@gmail.com>
2023-03-29 10:26:57 +0900

    Feature: 클라이언트 토큰 Business Logic 추가

--- bigdata-api/mata-mysql/src/main/java/com/ssafy/service/KafkaProducerService.java
+++ bigdata-api/mata-mysql/src/main/java/com/ssafy/service/KafkaProducerService.java
@@ -1,7 +1,12 @@
 package com.ssafy.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.ssafy.common.validation.RedisKeyExecption;
 import com.ssafy.dto.WebLog;
+import com.ssafy.dto.project.exception.NoSuchProjectException;
+import com.ssafy.entity.Project;
+import com.ssafy.repository.project.ProjectRepository;
+import com.ssafy.common.validation.Validation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -9,10 +14,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.util.concurrent.ListenableFutureCallback;
-
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
@@ -22,6 +26,9 @@ public class KafkaProducerService {
     @Value(value = "${spring.kafka.topic.tag-manager}")
     private String TOPIC_TAG_MANAGER;
     private final KafkaTemplate<String, String> template;
+    private final Validation validation;
+    private final ProjectRepository projectRepository;
+
 
     public void sendToKafka(final WebLog data) throws JsonProcessingException {
         final ProducerRecord<String, String> record = data.toProducerRecord(TOPIC_TAG_MANAGER, 0);
@@ -40,5 +47,11 @@ public class KafkaProducerService {
             }
         });
     }
+    @Transactional(readOnly = true)
+    public void checkValidation(String token){
+        if(!validation.checkRedisValidation(token)){
+            Project project = projectRepository.findByToken(token).orElseThrow(RedisKeyExecption::new);
+        };
 
+    }
 }
