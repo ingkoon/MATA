e63475530eb3203ccfbaaf0bdc112753f9a2be54
malachai <prussian1933@naver.com>
2023-03-29 20:58:51 +0900

    Feat: 시작하기 내용 추가

--- vue3/src/views/users/getting_started.vue
+++ vue3/src/views/users/getting_started.vue
@@ -12,6 +12,37 @@
                                         <div class="widget layout-spacing my-3">
                                             <div class="widget-heading">
                                                 <h3>
+                                                    시작하기에 앞서
+                                                </h3>
+                                            </div>
+                                            <div class="widget-content">
+                                                <div class="widget-content m-2 ">
+                                                    <p>
+                                                        웹서비스에 진입하는 고객의 로그를 저희 서비스가 수집할 수 있게 하기 위해선 몇가지 단계가 필요합니다.
+                                                    </p>
+                                                    <hr class='bg-secondary opacity-25' style='height: 2px'/>
+                                                    <h5>
+                                                        사용자 토큰 발급받기
+                                                    </h5>
+                                                    <p>
+                                                        효과적인 데이터 집계와 불필요한 수집을 하지 않기 위해 저희 서비스를 이용해주시는 사용자들께 토큰은 발급하고 있습니다.
+                                                        이는 이후 서비스 이용을 위해 필수적으로 코드에 주입해야 하니 참고해주시길 바랍니다.
+                                                    </p>
+                                                    <p>
+                                                        토큰이 도난당한다면 데이터의 집계가 부정확해질 수 있으니, 주기적으로 토큰을 재발급 받아 서비스에 적용해주시길 바랍니다.
+                                                    </p>
+                                                    <p>
+                                                        사용자님께 발급된 토큰은 다음과 같습니다.
+                                                    </p>
+                                                    <div class='p-1 inline'>
+                                                        <p>{{ "blahblah" }}</p>    
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="widget layout-spacing my-3">
+                                            <div class="widget-heading">
+                                                <h3>
                                                     <span style="font-weight:bold">Common JS</span>에서 시작하기 
                                                 </h3>
                                             </div>
@@ -104,6 +135,11 @@
             handleNewLine(str) {
                 return String(str).replace(/(?:\r\n|\r|\n)/g,"</br>");
             },
+            async getClientToken() {
+                let resp = await fetch()
+                let body = resp.json()
+                return body.token
+            }
         },
         data(){
             return{
