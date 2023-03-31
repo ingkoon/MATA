d68059c3bb4bfd88b4568a93bfe2f44a0749d574
junho <blacktick@naver.com>
2023-03-31 12:55:30 +0900

    Feat: 누적 테이블 90% 완성 test 시도...

--- hadoop-cluster/lib/apache-airflow-2.5.0/dags/cassandra_to_hive_workflow.py
+++ hadoop-cluster/lib/apache-airflow-2.5.0/dags/cassandra_to_hive_workflow.py
@@ -8,7 +8,7 @@ from lib.spark.Batching_Jobs import batching_hive, batching_cassandra_spark, bat
 
 default_args = {
     'owner': 'airflow',
-    'start_date': datetime(2023, 3, 30, 4, 30),
+    'start_date': datetime(2023, 3, 31, 2, 0),
     'retries': 1,
     'retry_delay': timedelta(minutes=1),
 }
@@ -59,49 +59,44 @@ dag12h = DAG(
     'cassandra_to_hive_12h',
     default_args=default_args,
     description='Cassandra to Hive',
-    schedule_interval=timedelta(hours=12)
+    schedule_interval=timedelta(minutes=5)
+    # schedule_interval=timedelta(hours=12)
 )
 
 dag1d = DAG(
     'hive_to_hive_1d',
     default_args=default_args,
     description='Hive to Hive',
-    schedule_interval= timedelta(days=1)
+    schedule_interval=timedelta(minutes=10)
+    # schedule_interval= "@daily"
 )
 
 dag1w = DAG(
     'hive_to_hive_1w',
     default_args=default_args,
     description='Hive to Hive',
-    schedule_interval=timedelta(weeks=1)
+    schedule_interval= "@weekly"
 )
 
 dag1mo = DAG(
     'hive_to_hive_1mo',
     default_args=default_args,
     description='Hive to Hive',
-    schedule_interval=relativedelta(months=1)
+    schedule_interval= "@monthly"
 )
 
-dag6mo = DAG(
-    'hive_to_hive_6mo',
-    default_args=default_args,
-    description='Hive to Hive',
-    schedule_interval=relativedelta(months=6)
-)
+# dag6mo = DAG(
+#     'hive_to_hive_6mo',
+#     default_args=default_args,
+#     description='Hive to Hive',
+#     schedule_interval=relativedelta(months=6)
+# )
 
 dag1y = DAG(
     'hive_to_hive_1y',
     default_args=default_args,
     description='Hive to Hive',
-    schedule_interval=relativedelta(years=1)
-)
-
-dagall = DAG(
-    'hive_to_hive_all',
-    default_args=default_args,
-    description='Hive to Hive',
-    schedule_interval=timedelta(days=1)
+    schedule_interval= "@yearly"
 )
 
 
@@ -175,8 +170,8 @@ hive_to_spark_1d = PythonOperator(
     task_id='hive_to_spark_1d',
     python_callable=batching_hive,
     op_kwargs = {"base_time" : now,
-                 "amount" : 1,
-                 "unit" : "d"},
+                 "amount" : 10,
+                 "unit" : "m"},
     dag=dag1d
 )
 
@@ -198,14 +193,14 @@ hive_to_spark_1mo = PythonOperator(
     dag=dag1mo
 )
 
-hive_to_spark_6mo = PythonOperator(
-    task_id='hive_to_spark_6mo',
-    python_callable=batching_hive,
-    op_kwargs = {"base_time" : now,
-                 "amount" : 6,
-                 "unit" : "mo"},
-    dag=dag6mo
-)
+# hive_to_spark_6mo = PythonOperator(
+#     task_id='hive_to_spark_6mo',
+#     python_callable=batching_hive,
+#     op_kwargs = {"base_time" : now,
+#                  "amount" : 6,
+#                  "unit" : "mo"},
+#     dag=dag6mo
+# )
 
 hive_to_spark_1y = PythonOperator(
     task_id='hive_to_spark_1y',
@@ -220,13 +215,12 @@ hive_to_spark_all = PythonOperator(
     task_id='hive_to_spark_all',
     python_callable=batching_hive_all,
     op_kwargs = {"base_time" : now,
-                 "amount" : 10000,
-                 "unit" : "d"},
-    dag=dagall
+                 "unit" : "all"},
+    dag=dag1d
 )
 
 
 
+hive_to_spark_1d >> hive_to_spark_all
 
-
-# cassandra_to_spark_1m, cassandra_to_spark_5m, cassandra_to_spark_10m, cassandra_to_spark_30m, cassandra_to_spark_1h, cassandra_to_spark_6h, cassandra_to_spark_12h
+# cassandra_to_spark_5m >> cassandra_to_spark_10m >> cassandra_to_spark_30m >> cassandra_to_spark_1h >> cassandra_to_spark_6h >> cassandra_to_spark_12h >> hive_to_spark_1d >> hive_to_spark_all
