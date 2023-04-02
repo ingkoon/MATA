14b0f42676711f594adeee7fa6989ede25b12a11
ktyoo226 <ktyoo226@naver.com>
2023-04-03 05:33:06 +0900

    feat : 히트맵 페이지 따로 제작중... 사이트 가져와서 그위에 점찍어보는건 성

--- vue3/src/router/index.js
+++ vue3/src/router/index.js
@@ -14,6 +14,7 @@ const routes = [
     { path: '/', name: 'Welcome', component: Welcome, meta: { layout: 'hero' }},
     { path: '/service/:id', name: 'Home', component: Home, meta: { layout: 'app' } },
     // { path: '/index2', name: 'index2', component: () => import(/* webpackChunkName: "index2" */ '../views/index2.vue'), },
+    { path: '/index3', name: 'index3', component: () => import(/* webpackChunkName: "index3" */ '../views/index3.vue'), },
 
     
     //auth
---
+++ vue3/src/views/index3.vue
@@ -0,0 +1,54 @@
+<template>
+  <div  ref="heatmapTarget">
+    <iframe ref="iframeRef" src="http://localhost:3001/" width="100%" height="800"></iframe>
+  </div>
+</template>
+
+<script>
+import { ref, onMounted } from 'vue';
+import heatmap from 'heatmap.js';
+
+export default {
+  setup() {
+    const heatmapTarget = ref(null);
+    let heatmapInstance = null
+
+    const heatmap1 = ref(null);
+    
+    onMounted(() => {
+      heatmapInstance = heatmap.create({
+        container: heatmapTarget.value,
+        // container: heatmap1.value,
+        
+        radius: 25,
+        maxOpacity: 0.6,
+        minOpacity: 0,
+        blur: 0.75,
+        gradient: {
+          '.3': 'blue',
+          '.5': 'green',
+          '.7': 'red'
+        }
+
+        
+      })
+      const data = [
+        { x: 100, y: 20, value: 100 },
+        { x: 101, y: 20, value: 100 },
+      ]
+
+
+
+      heatmapInstance.setData({
+        max:100,
+        data
+      })
+    });
+    
+    return {
+      heatmapTarget
+      // heatmap1
+    };
+  }
+}
+</script>
