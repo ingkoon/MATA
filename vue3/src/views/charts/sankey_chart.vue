11212fd052b42abad706ff7baa7d4cc73d27786a
junho <blacktick@naver.com>
2023-04-04 00:25:34 +0900

    Feat: 여정지도 ing...2222222222222

--- vue3/src/views/charts/sankey_chart.vue
+++ vue3/src/views/charts/sankey_chart.vue
@@ -19,6 +19,7 @@
                 serviceId: null,
                 accessToken: localStorage.getItem("accessToken"),
                 clientToken: null,
+                curNode: null,
             });
             const svgRef = ref(null);
             const items = {
@@ -36,41 +37,66 @@
                     { source: 'node0', target: 'node4', value: 3, color: 'red' },
                 ],
             };
+            let svg = null;
 
             onMounted(() => {
                 // 기본 페이지
                 store.state.serviceId = route.path.split('/')[2]
-                store.state.journals.curNode = "/"
-                console.log("serviceId.................", store.state.serviceId)
+                // store.state.journals.curNode = "/"
                 getJournalsInfo();
                 console.log("axios exit.............." + store.state.journals.curNode)
-                drawgraph();
             });
 
             watch(() => route.path, (newServiceId, oldServiceId) => {
                 // 페이지 변경 감지, curNode를 기본주소로...
                 store.state.serviceId = route.path.split('/')[2]
-                store.state.journals.curNode = "/"
                 console.log("serviceId.................", store.state.serviceId)
                 getJournalsInfo();
-                console.log("axios exit.............." + store.state.journals.curNode)
-                drawgraph();
             })
 
             watch(() => store.state.journals.curNode, () => {
-                // 현재 노드 변경
-                console.log("curNode..................", store.state.journals.curNode)
-                changeNodeAndLink();
-                drawgraph();
+                if(store.state.journals.curNode !== "/") {
+                    // 현재 노드 변경
+                    console.log("curNode..................", store.state.journals.curNode)
+                    drawgraph(store.state.journals.curNode);
+                }
             })
 
 
-            function changeNodeAndLink() {
+            async function changeNodeAndLink(data) {
                 // 노드, 링크 변경 -> 그림 다시 그리기
+                // items.nodes = []
+                // items.links = []
+                
+                const fromNode = store.state.journals.curNode;
+                console.log("fromNode......................",fromNode)
+                console.log(data)
+                items.nodes.push({name: fromNode, id: fromNode})
+                data.forEach((d) => {
+                    items.nodes.push({name: d.locationTo, id: d.locationTo})
+                    items.links.push({source: fromNode, target: d.locationTo, value: d.totalJournals})
+                })
+                
+                console.log(items.nodes)
+
+                // nodes: [
+                //     { node: 0, name: 'node0', id: 'node0', color: 'red' },
+                //     { node: 1, name: 'node1', id: 'node1', color: 'orange' },
+                //     { node: 2, name: 'node2', id: 'node2', color: 'blue' },
+                //     { node: 3, name: 'node3', id: 'node3', color: 'green' },
+                //     { node: 4, name: 'node4', id: 'node4', color: 'brown' },
+                // ],
+                //     links: [
+                //     { source: 'node0', target: 'node2', value: 1, color: 'red' },
+                //     { source: 'node1', target: 'node2', value: 2, color: 'orange' },
+                //     { source: 'node1', target: 'node3', value: 2, color: 'orange' },
+                //     { source: 'node0', target: 'node4', value: 3, color: 'red' },
+                // ],
+                
             }
 
             const getJournalsInfo = async () => {
-                console.log("getComponentStats()..........")
+                console.log("getJournalsInfo..........axios")
                 let resp = await axios({
                     method:'get',
                     url: process.env.VUE_APP_API_HOST+`/api/v1/weblog/journals?basetime=${Date.now()}&interval=all&serviceid=${store.state.serviceId}`,
@@ -78,22 +104,54 @@
                         "Authorization": `Bearer ${state.accessToken}`,
                     },
                 })
-                let body = resp.data;
-                for (let i = 0; i < body.length; i++) {
-                    console.log(body[i])
+                let data = resp.data;
+                
+                // Initialize an empty object to hold the grouped data
+                const groupedData = {};
+                
+                // Iterate through the array of data
+                data.forEach((item) => {
+                    // Check if the locationFrom already exists in the groupedData object
+                    if (groupedData[item.locationFrom]) {
+                        // If it exists, update the existing object with the new values
+                        groupedData[item.locationFrom].push({
+                            "locationTo":item.locationTo, 
+                            "totalJournals":item.totalJournal
+                        });
+                    } else {
+                        // If it doesn't exist, create a new object with the values
+                        groupedData[item.locationFrom] = [{
+                            "locationTo": item.locationTo,
+                            "totalJournals": item.totalJournal,
+                        }];
+                    }
+                });
+                let shortestKey;
+                for (let key in groupedData) {
+                    if (shortestKey != "None" || !shortestKey || key.length < shortestKey.length) {
+                        shortestKey = key;
+                    }
                 }
-
+                // console.log(groupedData[shortestKey])
+                
+                // 기본 세팅
+                store.state.journals.curNode = shortestKey;
+                
+                drawgraph(groupedData[shortestKey])
             }
 
-            function drawgraph() {
-                changeNodeAndLink();
+            async function drawgraph(data) {
+                console.log("grawgraph..................")
+                svg = null;
+                
+                await changeNodeAndLink(data);
                 const width = 600;
                 const height = 800;
                 const nodeWidth = 80;
                 const nodeHeight = 160;
                 const nodePadding = 200;
                 const ENABLE_LINKS_GRADIENTS = true;
-                const svg = d3.select(svgRef.value).attr('viewBox', [0, -100, width, height + 200]);
+                svg = d3.select(svgRef.value).attr('viewBox', [0, -100, width, height + 200]);
 
                 const s = sankey()
                     .nodeId((d) => d.name)
