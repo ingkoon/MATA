af5d53f3657cc003a9e64e874f830340327e2947
dsrla123 <dsrla123@naver.com>
2023-04-04 19:37:29 +0900

    feat: 컴포넌트별 클릭 바, 원차트

--- vue3/src/views/component_bar_chart.vue
+++ vue3/src/views/component_bar_chart.vue
@@ -7,6 +7,7 @@
 <script>
     import { onMounted, ref } from 'vue';
     import Plotly from 'plotly.js-dist';
+    import axios from 'axios';
 
     export default {
         name: 'BarChart',
@@ -17,9 +18,42 @@
                 const data = [{ x: ['giraffes', 'orangutans', 'monkeys'], y: [20, 14, 23], type: 'bar' }];
                 const layout = { title: 'A Simple Bar Chart' };
                 const config = { responsive: true };
-                
                 console.log(data)
 
+                axios({
+                    method:'get',
+                    url: process.env.VUE_APP_API_HOST+'/api/v1/weblog/components?basetime=20230331085028&interval=1h&serviceid=2',
+
+                })
+                    .then(res=>{
+                        console.log(res)
+                        const components=[]
+                        const targetCounts={}
+                        for (const iterator of res.data) {
+                            console.log(iterator.targetId)
+                            const targetId=iterator.targetId;
+                            if(targetId in targetCounts){
+                                targetCounts[targetId]++;
+                            } else{
+                                targetCounts[targetId]=1;
+                            }
+                        }
+                        const result=[]
+                        for ( const[targetId,count] of Object.entries(targetCounts)){
+                            result.push(count);
+                            components.push(targetId)
+                        }
+                        console.log("컴포넌트 중복제거는:",components)
+                        data[0].x=components
+                        data[0].y=result
+                        console.log("변형 후 data",data,result)
+                        Plotly.newPlot(barChart.value, data, layout, config);
+
+                    })
+                    .catch(err=>{
+                        console.log(err)
+                    })
+                
                 Plotly.newPlot(barChart.value, data, layout, config);
             });
 
