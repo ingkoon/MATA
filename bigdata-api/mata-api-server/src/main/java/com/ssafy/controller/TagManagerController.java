a455f05440cfa44ba538c46f430eef49c79780ff
junho <blacktick@naver.com>
2023-04-05 13:46:17 +0900

    Feat: weblog dummy data...

--- bigdata-api/mata-api-server/src/main/java/com/ssafy/controller/TagManagerController.java
+++ bigdata-api/mata-api-server/src/main/java/com/ssafy/controller/TagManagerController.java
@@ -4,6 +4,7 @@ import com.ssafy.dto.WebLog;
 import com.ssafy.service.KafkaProducerService;
 import lombok.RequiredArgsConstructor;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +12,7 @@ import java.sql.Timestamp;
 import java.util.*;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class TagManagerController {
@@ -31,8 +33,8 @@ public class TagManagerController {
         }
 
         Arrays.stream(body).forEach(wl -> {
-//            kafkaProducerService.checkValidation(wl.getServiceToken()); // 토큰 검증 로직
-//            wl.setServiceId(kafkaProducerService.getProjectId(wl.getServiceToken())); // 토큰으로 서비 아이디 가져오기
+            kafkaProducerService.checkValidation(wl.getServiceToken()); // 토큰 검증 로직
+            wl.setServiceId(kafkaProducerService.getProjectId(wl.getServiceToken())); // 토큰으로 서비 아이디 가져오기
             System.out.println(wl.getServiceToken());
             System.out.println(wl.getSessionId());
             System.out.println(wl.getEvent());
@@ -55,8 +57,39 @@ public class TagManagerController {
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
-    @PostMapping("/exampledata_webtojava")
+    @GetMapping("/exampledata_webtojava")
     public ResponseEntity<?> getLogDump() {
+
+        List referlist = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            referlist.add("https://www.google.com/" + ("mata"+i).hashCode());
+        }
+        for (int i = 0; i < 100; i++) {
+            referlist.add("https://www.naver.com/" + ("mata"+i).hashCode());
+        }
+        for (int i = 0; i < 100; i++) {
+            referlist.add("https://www.daum.com/" + ("mata"+i).hashCode());
+        }
+
+        List<String> urlList = new ArrayList<>();
+        // 10개
+        urlList.add("/");
+        urlList.add("/first");
+        urlList.add("/second");
+        urlList.add("/first/abcabc");
+        urlList.add("/first/shop");
+        urlList.add("/first/list");
+        urlList.add("/second/qna");
+        urlList.add("/second/board");
+        urlList.add("/second/map");
+        urlList.add("/journals");
+
+        List<String> idList = new ArrayList<>();
+        idList.add("button-back");
+        idList.add("button-event");
+        idList.add("map1");
+        idList.add("map2");
+
 //        web에서 들어오는 데이터 형태
 //        serviceToken: this.serviceToken,
 //        sessionId: this.sessionId,
@@ -69,13 +102,66 @@ public class TagManagerController {
 //        referrer: this.referrer,
 //        timestamp: Date.now(),
 //        pageDuration: Date.now() - this.enterTimer
-        for (int i = 0; i < 100000; i++) {
-            WebLog wl = new WebLog();
 
-            try {
-                kafkaProducerService.sendToKafka(wl);
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
+        // 1000명의 유저 접속, url 랜덤
+        for (int i = 0; i < 1000; i++) {
+            // 10개의 event
+            for (int j = 0; j < 10; j++) {
+                WebLog wl = new WebLog();
+                wl.setServiceId(2L);
+                int hashValue = (int)(Math.random()*100000);
+                int hashValue2 = (int)(Math.random()*100000);
+
+                wl.setServiceToken("token.........."+i);
+                wl.setSessionId(String.valueOf(String.valueOf(hashValue).hashCode()));
+                wl.setPrevLocation("none");
+                long nowTime = System.currentTimeMillis();
+                long time = nowTime - nowTime%1000000;
+
+                // 외부 접속
+                if(i%5 == 0) {
+                    wl.setPrevLocation("none");
+                    wl.setLocation("http://localhost:3001"+urlList.get(i%3));
+                } else {
+                    // 내부 이동
+                    wl.setPrevLocation("http://localhost:3001"+urlList.get(hashValue%10));
+                    wl.setLocation("http://localhost:3001"+urlList.get(hashValue2%10));
+                }
+                long duTime = 10 + hashValue % 100;
+                int hashValue3 = (int)(Math.random()*100000);
+                wl.setTimestamp(time+hashValue*10);
+                wl.setEvent("none");
+                if(j==0) {
+                    // pageenter
+                    wl.setEvent("pageenter");
+                    wl.setPageDuration(0);
+                    wl.setPositionX(0);
+                    wl.setPositionY(0);
+                } else if(j==9) {
+                    // pageleave
+                    wl.setEvent("pageleave");
+                    wl.setPageDuration(duTime*j);
+                    wl.setPositionX(0);
+                    wl.setPositionY(0);
+                } else {
+                    // click
+                    wl.setTargetId(idList.get(hashValue3%4));
+                    wl.setEvent("click");
+                    wl.setPageDuration(duTime*j);
+                    wl.setPositionX(hashValue%1000 + hashValue3%100);
+                    wl.setPositionY(hashValue%520 + hashValue3%100);
+                }
+
+                if(wl.getPrevLocation().equals("none")) {
+                    // none -> 특정 주소로 변경...
+                    wl.setReferrer((String) referlist.get((int) (Math.random() * 300)));
+                }
+                try {
+                    kafkaProducerService.sendToKafka(wl);
+                    log.info("input" + i +" "+ j);
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
             }
         }
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
