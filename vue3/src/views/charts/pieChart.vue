45671f5ac52b7a6d5afa4ba21350c2cd8a4a9491
박건우 <scv74502@naver.com>
2023-04-05 15:35:57 +0900

    pie chart

--- vue3/src/views/charts/pieChart.vue
+++ vue3/src/views/charts/pieChart.vue
@@ -1,56 +1,106 @@
 <template>
     <div>
-        <h1>Donut Chart</h1>
-        <apexchart type="pie" :options="chartOptions" :series="chartSeries"></apexchart>
+        <div ref="pieChart"></div>
     </div>
 </template>
 
 <script>
+    import axios from 'axios';
+    import { ref, watch, onMounted, reactive, watchEffect, onUpdated } from 'vue';
+    import { useRoute } from 'vue-router';
+    import ApexCharts from 'apexcharts';
+    import { useStore } from 'vuex';
+
     export default {
-        components:{
-            name: 'donutChart',
-        },
-        data() {
-            return {
-                chartOptions: {
-                    chart: {
-                        type: 'pie'
+        setup() {
+            const store = useStore();
+            const route = useRoute();
+            const state = reactive({
+                serviceId: null,
+                accessToken: localStorage.getItem("accessToken"),
+                clientToken: null,
+                curNode: null,
+            });
+            const pieChart = ref(null);
+            const items = {
+                nodes: [],
+                links: [],
+            };
+
+            // console.log("url : " + process.env.VUE_APP_API_HOST+`/api/v1/weblog/refersall?basetime=${Date.now()}&interval=1h&serviceid=${store.state.serviceId}`);
+            onMounted(async () => {
+                store.state.serviceId = route.path.split('/')[2];
+                let response = await axios({
+                    method:'get',
+                    url: process.env.VUE_APP_API_HOST+`/api/v1/weblog/refersall?basetime=${Date.now()}&interval=all&serviceid=2`,
+                    headers:{
+                        "Authorization": `Bearer ${state.accessToken}`,
                     },
-                    labels: [],
-                    responsive: [
-                        {
-                            breakpoint: 480,
-                            options: {
-                                chart: {
-                                    width: 200
-                                },
-                                legend: {
-                                    position: 'bottom'
-                                }
+                })
+                
+                
+                console.log("------------response----------------");
+                console.log(response);
+                console.log("------------response.data----------------");
+
+                console.log(response.data);
+                const data = response.data;
+
+                // Calculate the pageenterPercentage for each object in the data
+                data.forEach(obj => {
+                    obj.pageenterPercentage = obj.totalPageenter / obj.totalSession;
+                });
+
+                // Group the data by referrer
+                const referrerGroups = data.reduce((groups, item) => {
+                    const referrer = item.referrer;
+                    const existingGroup = groups.find(group => group.x === referrer);
+                    if (existingGroup) {
+                        existingGroup.y += item.totalPageenter;
+                    } else {
+                        if (referrer !== null) {
+                            groups.push({
+                                x: referrer,
+                                y: item.totalPageenter
+                            });
+                        }
+                    }
+                    return groups;
+                }, []);
+                
+                
+                console.log("------------referrerGroups----------------");
+                console.log(referrerGroups);
+
+                const chartData = referrerGroups.filter(group => group.x !== null);
+
+                const options = {
+                    series: chartData.map(data => data.y),
+                    chart: {
+                        type: 'pie',
+                        height: 350
+                    },
+                    labels: chartData.map(data => data.x),
+                    responsive: [{
+                        breakpoint: 480,
+                        options: {
+                            chart: {
+                                width: 200
+                            },
+                            legend: {
+                                position: 'bottom'
                             }
                         }
-                    ]
-                },
-                chartSeries: []
-            };
-        },
-        mounted() {
-            console.log('before fetch');
-            console.log(Date.now());
-            fetch(process.env.VUE_APP_API_HOST + `/api/v1/weblog/refersall?basetime=${Date.now()}&interval=all&serviceid=2`)
-                .then(response => response.json())
-                .then(data => {
-                    const labels = [];
-                    const series = [];
-                    
-                    data.forEach(item => {
-                        labels.push(item.referrerId);
-                        series.push(item.totalSession);
-                    });
+                    }]
+                };
 
-                    this.chartOptions.labels = labels;
-                    this.chartSeries = series;
-                });
+                const chartInstance = new ApexCharts(pieChart.value, options);
+                await chartInstance.render();
+            });
+
+            return {
+                pieChart
+            };
         }
     };
 </script>
